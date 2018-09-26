3cccde47a5fa4c3315d132c5df1a4627097d8f0b
Nilantha, Dambadeni Kalu Achchillage <s528116@nwmissouri.edu>
2018-09-26 01:52:12 -0500

    Implement the login functionality - not working

--- app/src/main/java/com/example/s528116/smartinventory_test/MainActivity.java
+++ app/src/main/java/com/example/s528116/smartinventory_test/MainActivity.java
@@ -3,11 +3,86 @@ package com.example.s528116.smartinventory_test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText emailET, passwordET;
+    private FirebaseAuth auth;
+    //private progressBar progressBar;
+    private Button loginBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser()!=null){
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
+            finish();
+        }
+
+        setContentView(R.layout.activity_main);
+        emailET = findViewById(R.id.emailTXT);
+        passwordET = findViewById(R.id.passwordTXT);
+        loginBTN = findViewById(R.id.loginBTN);
+
+        auth = FirebaseAuth.getInstance();
+        loginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailET.getText().toString();
+                final String password = passwordET.getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(MainActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(MainActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+//                                progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        passwordET.setError("minimum letters");
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "failed", Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                        });
+            }
+        });
     }
 }
